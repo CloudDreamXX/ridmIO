22456462e15b15d4751af9d3605b8a1cf5d0a49f
CloudDreamXX <132334368+CloudDreamXX@users.noreply.github.com>
2025-02-07 14:38:56 +0200

    height issue

--- src/pages/home/ui.tsx
+++ src/pages/home/ui.tsx
@@ -14,39 +14,6 @@ import {
 } from "widgets/home";
 
 export const Home: React.FC = () => {
-  useEffect(() => {
-    const isIOSSafari =
-      /iPhone|iPod|iPad/i.test(navigator.userAgent) &&
-      /WebKit/i.test(navigator.userAgent) &&
-      !/(CriOS|FxiOS|OPiOS|mercury)/i.test(navigator.userAgent);
-
-    if (isIOSSafari) {
-      document.documentElement.style.setProperty("scroll-behavior", "auto");
-
-      const smoothScroll = (e: Event) => {
-        e.preventDefault();
-        const href = (e.currentTarget as HTMLAnchorElement).getAttribute(
-          "href"
-        );
-        if (href) {
-          document.querySelector(href)?.scrollIntoView({
-            behavior: "smooth",
-          });
-        }
-      };
-
-      document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-        anchor.addEventListener("click", smoothScroll);
-      });
-
-      return () => {
-        document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-          anchor.removeEventListener("click", smoothScroll);
-        });
-      };
-    }
-  }, []);
-
   return (
     <div className={styles.container}>
       <Hero />
--- src/shared/lib/hooks/useScrollToView.tsx
+++ src/shared/lib/hooks/useScrollToView.tsx
@@ -11,4 +11,37 @@ export const useScrollToView = () => {
       return;
     }
   }, [pathname, hash]);
+
+  useEffect(() => {
+    const isIOSSafari =
+      /iPhone|iPod|iPad/i.test(navigator.userAgent) &&
+      /WebKit/i.test(navigator.userAgent) &&
+      !/(CriOS|FxiOS|OPiOS|mercury)/i.test(navigator.userAgent);
+
+    if (isIOSSafari) {
+      document.documentElement.style.setProperty("scroll-behavior", "auto");
+
+      const smoothScroll = (e: Event) => {
+        e.preventDefault();
+        const href = (e.currentTarget as HTMLAnchorElement).getAttribute(
+          "href"
+        );
+        if (href) {
+          document.querySelector(href)?.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+      };
+
+      document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", smoothScroll);
+      });
+
+      return () => {
+        document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+          anchor.removeEventListener("click", smoothScroll);
+        });
+      };
+    }
+  }, []);
 };
