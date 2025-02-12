b4cbfd60c981355de51cbce5146a0cc8460d37df
CloudDreamXX <132334368+CloudDreamXX@users.noreply.github.com>
2025-02-12 14:16:12 +0200

    edits 12/02

--- src/shared/lib/hooks/useScrollToView.tsx
+++ src/shared/lib/hooks/useScrollToView.tsx
@@ -1,16 +1,39 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 
+const smoothScroll = (element: HTMLElement) => {
+  const elementPosition = element.getBoundingClientRect().top;
+  const offsetPosition = elementPosition + window.pageYOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth",
+  });
+};
+
 export const useScrollToView = () => {
   const { pathname, hash } = useLocation();
 
   useEffect(() => {
     if (hash && pathname === "/") {
       const anchor = document.getElementById(hash.slice(1));
-      anchor?.scrollIntoView({
-        behavior: "smooth",
-      });
+
+      if (anchor) {
+        const isIOSSafari =
+          /iPhone|iPod|iPad/i.test(navigator.userAgent) &&
+          /WebKit/i.test(navigator.userAgent) &&
+          !/(CriOS|FxiOS|OPiOS|mercury)/i.test(navigator.userAgent);
+
+        if (isIOSSafari) {
+          smoothScroll(anchor);
+        } else {
+          anchor.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+      }
       return;
     }
+    window.scrollTo(0, 0);
   }, [pathname, hash]);
 };
